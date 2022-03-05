140c6b618382d8cde379cd2ca6ef57c7f44ddf18
Ivan Galban <ivan.galban.smith@gmail.com>
2022-03-04 20:41:35 -0600

    Solve problem 53

---
+++ src/0053.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int sum = nums[0], ans = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      if (sum < 0)
+        sum = 0;
+
+      sum += nums[i];
+      ans = max(sum, ans);
+    }
+    return ans;
+  }
+};
