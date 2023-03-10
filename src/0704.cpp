3d41f90351c4f1d232387f1179aa8aae753a74e1
Ivan Galban <ivan.galban.smith@gmail.com>
2023-03-10 13:58:39 -0600

    Solve 704

---
+++ src/0704.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size()-1;
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if(nums[mid] == target)
+              return mid;
+            else if(nums[mid] < target)
+              lo = mid + 1;
+            else
+              hi = mid-1;
+        }
+        return -1;
+    }
+};
