914040580a3997c7d5113a3ba4b7b84ee9270040
Ivan Galban <ivan.galban.smith@gmail.com>
2024-01-02 09:52:18 -0600

    Solve LCA in BT

---
+++ src/0236.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == NULL || root == p || root == q)
+            return root;
+
+        auto left = lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL)
+            return right;
+        if (right == NULL)
+            return left;
+
+        return root;
+    }
+};
