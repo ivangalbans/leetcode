18073a513ae8a4ddd8cb187c415a600f7bb214bf
Ivan Galban <ivan.galban.smith@gmail.com>
2024-01-13 10:10:32 -0600

    Solve merge sorted array

--- src/0088.cpp
+++ src/0088.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/merge-sorted-array/description/
 
+// Memory O(m + n)
+
 class Solution
 {
 public:
@@ -16,5 +18,23 @@ public:
         while (r < n)
             temp[index++] = nums2[r++];
         copy(begin(temp), end(temp), begin(nums1));
+    }
+};
+
+// Memory O(1)
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0)
+        {
+            if (i >= 0 && nums1[i] >= nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
     }
 };
